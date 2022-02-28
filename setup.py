ae6077bbafe82d4e8637dba29e4410c4c7aed1db
Aaron Stopher <aaron.stopher@gmail.com>
2022-02-28 02:39:40 -0700

    getlogger fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="EZLogger",
+    name="EZ_Logger",
     version="0.0.1",
     author="Aaron Stopher",
     description="Easily configure, simple granular loggers for all your modules.",
